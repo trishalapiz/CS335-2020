bd13cb650be39d5aa8ffc7ca686a0bfef81e8b56
Trisha Lapiz <trisha.lapiz@gmail.com>
2020-08-02 18:30:49 +1200

    update div tag and unicode

--- A1/tlap632.js
+++ A1/tlap632.js
@@ -1,39 +1,39 @@
 function home() {
-    document.getElementById('one').style.display = "inline";
-    document.getElementById('two').style.display = "none";
-    document.getElementById('three').style.display = "none";
-    document.getElementById('four').style.display = "none";
-    document.getElementById('five').style.display = "none";
+    document.getElementById('home').style.display = "inline";
+    document.getElementById('products').style.display = "none";
+    document.getElementById('location').style.display = "none";
+    document.getElementById('news').style.display = "none";
+    document.getElementById('guest').style.display = "none";
 }
 
 function products() {
-    document.getElementById('one').style.display = "none";
-    document.getElementById('two').style.display = "inline";
-    document.getElementById('three').style.display = "none";
-    document.getElementById('four').style.display = "none";
-    document.getElementById('five').style.display = "none";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('products').style.display = "inline";
+    document.getElementById('location').style.display = "none";
+    document.getElementById('news').style.display = "none";
+    document.getElementById('guest').style.display = "none";
 }
 
 function locations() {
-    document.getElementById('one').style.display = "none";
-    document.getElementById('two').style.display = "none";
-    document.getElementById('three').style.display = "inline";
-    document.getElementById('four').style.display = "none";
-    document.getElementById('five').style.display = "none";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('products').style.display = "none";
+    document.getElementById('location').style.display = "inline";
+    document.getElementById('news').style.display = "none";
+    document.getElementById('guest').style.display = "none";
 }
 
 function news() {
-    document.getElementById('one').style.display = "none";
-    document.getElementById('two').style.display = "none";
-    document.getElementById('three').style.display = "none";
-    document.getElementById('four').style.display = "inline";
-    document.getElementById('five').style.display = "none";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('products').style.display = "none";
+    document.getElementById('location').style.display = "none";
+    document.getElementById('news').style.display = "inline";
+    document.getElementById('guest').style.display = "none";
 }
 
 function guestBook() {
-    document.getElementById('one').style.display = "none";
-    document.getElementById('two').style.display = "none";
-    document.getElementById('three').style.display = "none";
-    document.getElementById('four').style.display = "none";
-    document.getElementById('five').style.display = "inline";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('products').style.display = "none";
+    document.getElementById('location').style.display = "none";
+    document.getElementById('news').style.display = "none";
+    document.getElementById('guest').style.display = "inline";
 }
